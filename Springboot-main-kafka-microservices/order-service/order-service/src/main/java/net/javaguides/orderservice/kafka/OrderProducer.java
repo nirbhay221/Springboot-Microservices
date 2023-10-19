989035bbf17c5fa0b65e0a72cf516bf602a6fea2
Nirbhay Malhotra <nirbhaymalhotra2@gmail.com>
2023-10-19 12:29:41 -0400

    initial kafka message broker

---
+++ Springboot-main-kafka-microservices/order-service/order-service/src/main/java/net/javaguides/orderservice/kafka/OrderProducer.java
@@ -0,0 +1,29 @@
+package net.javaguides.orderservice.kafka;
+
+import net.javaguides.basedomains.dto.Order;
+import net.javaguides.basedomains.dto.OrderEvent;
+import org.apache.kafka.clients.admin.NewTopic;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.support.KafkaHeaders;
+import org.springframework.messaging.Message;
+import org.springframework.messaging.support.MessageBuilder;
+import org.springframework.stereotype.Service;
+
+@Service
+public class OrderProducer {
+    private static final Logger LOGGER = LoggerFactory.getLogger(OrderProducer.class);
+    private NewTopic newTopic;
+    private KafkaTemplate<String, OrderEvent> kafkaTemplate;
+
+    public OrderProducer(NewTopic newTopic, KafkaTemplate<String, OrderEvent> kafkaTemplate) {
+        this.newTopic = newTopic;
+        this.kafkaTemplate = kafkaTemplate;
+    }
+    public void sendMessage(OrderEvent event){
+        LOGGER.info(String.format("Order event --> %s",event));
+        Message<OrderEvent> message = MessageBuilder.withPayload(event).setHeader(KafkaHeaders.TOPIC,newTopic.name()).build();
+        kafkaTemplate.send(message);
+    }
+}
